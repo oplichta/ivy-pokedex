5e3e0ce1dfbe979e8325bba05d5e0b91035433e3
Oskar <oskar@rescompany.com>
2020-04-28 13:53:35 +0200

    Added pokemon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { PokemonComponent } from './pokemon-list/pokemon/pokemon.component';
 
 @NgModule({
-  declarations: [AppComponent, PokemonListComponent],
+  declarations: [AppComponent, PokemonListComponent, PokemonComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
